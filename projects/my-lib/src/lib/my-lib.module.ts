9bbb0a40c75506a638f80557f8cd735a1b92b8b6
vadzim <vadzim_plotnikau@epam.com>
2025-06-11 02:31:03 +0400

    master add only package.

--- projects/my-lib/src/lib/my-lib.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { HelloComponent } from './hello/hello.component';
-
-@NgModule({
-  declarations: [HelloComponent],
-  imports: [],
-  exports: [HelloComponent]
-})
-export class MyLibModule {}
--- projects/my-lib/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of my-lib
- */
-
-export * from './lib/hello/hello.component';
-export * from './lib/my-lib.module';
